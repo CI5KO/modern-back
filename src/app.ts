ac2343950721bb78c997f24c18cd5a80d726cb68
CI5KO <dev.hector.og@gmail.com>
2025-04-29 10:19:40 -0600

    feature/validate-cookie

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,31 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-// Enable CORS for all origins
-app.use(cors());
+const allowedOrigins = ["http://localhost:3051", "http://localhost:8080"];
 
-// Enable JSON body parsing
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (_request, response) => {
   response.status(200).send({ message: "Status ok" });
 });
 
-// user test:
-// email: test
-// password: uniat
 app.post("/login", (request, response) => {
   const {
     email,
@@ -27,9 +37,25 @@ app.post("/login", (request, response) => {
     return;
   }
 
-  const key: string = email + password;
+  const key: string = `${email}:${password}`;
 
   response.status(200).send({ message: `Hello ${email}`, key });
+});
+
+app.get("/hello", (request, response) => {
+  const token = request.cookies?.token;
+  if (!token) {
+    response.status(401).json({ message: "No token found in cookies" });
+    return;
+  }
+
+  const [email] = token.split(":");
+  if (!email) {
+    response.status(400).json({ message: "Invalid token format" });
+    return;
+  }
+
+  response.status(200).json({ message: `Hello! ${email}` });
 });
 
 export default app;
